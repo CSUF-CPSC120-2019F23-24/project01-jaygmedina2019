d49ee6daa645f1ddef6693dcc6a9f3fba88e130e
jaygmedina2019 <you@example.com>
2019-09-07 23:37:43 -0700

    this is what I have so far

--- main.cpp
+++ main.cpp
@@ -1,2 +1,26 @@
-// Name:
+// Name:Juan Medina
 // This program calculates and displays business expenses.
+#include <iostream>
+#include <string>
+#include <iomanip>
+int main()
+{
+  //creating variable to hold information from the user
+  std::string location;
+  double days;
+  double hotel_expenses;
+  double meal_expenses;
+  //Message that welcomes the user to the Business Trip Tracker
+  std::cout << "Welcome to the Business Trip Tracker!\n ";
+  //Blank line showed in example
+  std::cout <<std::endl;
+  //Message asking user to enter trip Location
+  std::cout << "What is the businees trip location? ";
+  //User Response being recorded
+  std::cin >> location;
+  //Message asking for the amount of days that the trip will last
+  std::cout <<"How many days will the trip take? ";
+  //User response being recorded for amount of days
+  std::cin >> days;
+  //git
+}
