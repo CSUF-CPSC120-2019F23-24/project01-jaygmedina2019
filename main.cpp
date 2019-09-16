2e20b15c8d17a0ecaec418d0ff89fb6ab56a4cd6
jaygmedina2019 <jayg.medina00@csu.fullerton.edu>
2019-09-16 15:40:57 -0700

    Updated coding style to match comments made by professor

--- main.cpp
+++ main.cpp
@@ -8,15 +8,14 @@
 int main()
 {
   //Variables to hold information from user
-
   std::string location;
   double days, hotel_expenses, meal_expenses, total_expenses;
 
   //Incoming Welcome Message
-  std::cout << "Welcome to the business Trip Tracker!\n" ;
+  std::cout << "Welcome to the business Trip Tracker!\n";
 
   //blank line showed in example
-  std::cout <<std::endl;
+  std::cout << std::endl;
 
   //Ask user for the location
   std::cout << "what is the business trip location? ";
@@ -26,30 +25,32 @@ int main()
   std::cout << "How many days will the trip take? ";
   std::cin >> days;
 
-  // Ask the user for the hotel expenses
+  //Ask the user for the hotel expenses
   std::cout << "What is the total hotel expense? ";
   std::cin >> hotel_expenses;
 
   //Ask the user for the meal expenses
-  std::cout <<"what is the meal expense?";
+  std::cout << "what is the meal expense?";
   std::cin >> meal_expenses;
 
-  std::cout<<std::endl;
+  //Additional extra line needed to look lile example
+  std::cout << std::endl;
+
   //Gather the total expenses
-  total_expenses = hotel_expenses + meal_expenses ;
+  total_expenses = hotel_expenses + meal_expenses;
 
   //print out the header with the data from the user
-  std::cout <<std::setw(12) <<"Location"
+  std::cout << std::setw(12) << "Location"
     << std::setw(8) << "Days"
     << std::setw(9) << "Hotel"
     << std::setw(8) << "Meal"
     << std::setw(9) << "Total" << std::endl;
 
-  std::cout <<std::setw(12) << location
-    <<std::setw(8) << days
-    <<std::setw(9) << hotel_expenses
-    <<std::setw(8) << meal_expenses
-    <<std::setw(9) << total_expenses << std::endl;
+  std::cout << std::setw(12) << location
+    << std::setw(8) << days
+    << std::setw(9) << hotel_expenses
+    << std::setw(8) << meal_expenses
+    << std::setw(9) << total_expenses << std::endl;
 
   return 0;
 }
